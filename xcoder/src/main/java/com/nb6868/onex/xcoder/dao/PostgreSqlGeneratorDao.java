25ad5966687003eb1f189e7afa87cf6498a9c08d
Charles <zhangchaoxu@gmail.com>
2020-07-30 09:06:24 +0800

    init xcoder

---
+++ xcoder/src/main/java/com/nb6868/onex/xcoder/dao/PostgreSqlGeneratorDao.java
@@ -0,0 +1,13 @@
+package com.nb6868.onex.xcoder.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * PostgreSQL代码生成器
+ *
+ * @author Charles (zhanngchaoxu@gmail.com)
+ */
+@Mapper
+public interface PostgreSqlGeneratorDao extends GeneratorDao {
+
+}
---
+++ xcoder/src/main/java/com/nb6868/onex/xcoder/utils/OnexException.java
@@ -0,0 +1,53 @@
+package com.nb6868.onex.xcoder.utils;
+
+/**
+ * 自定义异常
+ *
+ * @author Charles (zhangchoxu@gmail.com)
+ */
+public class OnexException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+    private String msg;
+    private int code = 500;
+
+    public OnexException(String msg) {
+		super(msg);
+		this.msg = msg;
+	}
+
+	public OnexException(String msg, Throwable e) {
+		super(msg, e);
+		this.msg = msg;
+	}
+
+	public OnexException(String msg, int code) {
+		super(msg);
+		this.msg = msg;
+		this.code = code;
+	}
+
+	public OnexException(String msg, int code, Throwable e) {
+		super(msg, e);
+		this.msg = msg;
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+
+}
