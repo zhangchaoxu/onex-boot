19669bb33c79077d103d1bd611f4d25c135237c6
Charles <zhangchaoxu@gmail.com>
2020-09-03 22:57:07 +0800

    fix Shiro issue

--- rocket/oneapi/src/main/java/com/nb6868/onex/common/config/ShiroConfig.java
+++ rocket/oneapi/src/main/java/com/nb6868/onex/common/config/ShiroConfig.java
@@ -9,10 +9,8 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import javax.servlet.Filter;
 import java.util.HashMap;
@@ -32,7 +30,6 @@ public class ShiroConfig {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
         sessionManager.setSessionValidationSchedulerEnabled(false);
         sessionManager.setSessionIdUrlRewritingEnabled(false);
-
         return sessionManager;
     }
 
@@ -83,35 +80,9 @@ public class ShiroConfig {
         // 除上述anon外,其它都需要过oauth2
         filterMap.put("/**", "shiro");
         // 加入注解中含有anon的
-        filterMap.putAll(getAnonAccessSet());
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
         return shiroFilter;
-    }
-
-    @Autowired
-    Environment env;
-
-    private Map<String, String> getAnonAccessSet() {
-        Map<String, String> filterRuleMap = new LinkedHashMap<>();
-
-        // 获得所有controller
-        /*String[] controllerNameList = SpringContextUtils.getBeanNamesForAnnotation(org.springframework.stereotype.Controller.class);
-        String[] restControllerNameList = SpringContextUtils.getBeanNamesForAnnotation(org.springframework.web.bind.annotation.RestController.class);
-
-        for (String controllerName : restControllerNameList) {
-            Class<?> clazz = SpringContextUtils.getType(controllerName);
-            Method[] methods = clazz.getMethods();
-            for (Method method : methods) {
-                AnonAccess annotation = method.getAnnotation(AnonAccess.class);
-                if (null != annotation) {
-                    filterRuleMap.put(annotation.path(), "anon");
-                }
-            }
-
-        }*/
-
-        return filterRuleMap;
     }
 
     @Bean("lifecycleBeanPostProcessor")
