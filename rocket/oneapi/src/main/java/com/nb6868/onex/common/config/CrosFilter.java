45352dcb7b403bc829ee7b0426285f1fbb910db3
Charles <zhangchaoxu@gmail.com>
2020-12-03 14:35:13 +0800

    fix springboot2.4.0 cros issue

--- rocket/oneapi/src/main/java/com/nb6868/onex/common/config/CrosFilter.java
+++ rocket/oneapi/src/main/java/com/nb6868/onex/common/config/CrosFilter.java
@@ -23,17 +23,15 @@ public class CrosFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "content-type,token,device");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        // Access-Control-Allow-Origin和Access-Control-Allow-Credentials有约束;Credentials true,Origin必须指定具体来源,不能用*通配;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader(HttpHeaders.ORIGIN));
+        response.setHeader("Access-Control-Max-Age", "3600");
         //  放行所有Options请求，提高接口访问速度
         if (RequestMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
-            HttpServletResponse response = (HttpServletResponse) resp;
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Headers", "content-type,token");
-            response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-            // Access-Control-Allow-Origin和Access-Control-Allow-Credentials有约束
-            // Credentials true,Origin必须指定具体来源,不能用*通配;
-            // Credentials false,Origin可以用*通配;
-            response.setHeader("Access-Control-Allow-Origin", request.getHeader(HttpHeaders.ORIGIN));
-            response.setHeader("Access-Control-Max-Age", "3600");
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, resp);
