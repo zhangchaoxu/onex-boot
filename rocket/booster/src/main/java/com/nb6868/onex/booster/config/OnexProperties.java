714ce9e1c434b63bd80ea84a8d1d05cb6653f519
Charles <zhangchaoxu@gmail.com>
2020-12-14 17:14:17 +0800

    add onex properties

---
+++ rocket/booster/src/main/java/com/nb6868/onex/booster/config/OnexProperties.java
@@ -0,0 +1,23 @@
+package com.nb6868.onex.booster.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * OneX相关配置文件
+ *
+ * @author Charles
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "onex")
+public class OnexProperties {
+
+    /**
+     * token生成策略
+     */
+    private String tokenPolicy;
+
+
+}
