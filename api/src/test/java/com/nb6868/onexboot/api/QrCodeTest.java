798738f8ee6b6df475ec863df71a3bbb6cee5a65
Charles <zhangchaoxu@gmail.com>
2021-01-18 17:13:55 +0800

    fix qrcode encode chinese issue add qrcode decode

--- api/src/test/java/com/nb6868/onexboot/api/QrCodeTest.java
+++ api/src/test/java/com/nb6868/onexboot/api/QrCodeTest.java
@@ -1,23 +1,22 @@
 package com.nb6868.onexboot.api;
 
 import com.nb6868.onexboot.api.common.util.QrCodeUtils;
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * 二维码测试
+ *
+ * @author Charles zhangchaoxu@gmail.com
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class QrCodeTest {
 
     @Test
-    public void generateQrcode() throws IOException {
-        File file = QrCodeUtils.from("Hello World").withSize(600, 800).file("qrtest");
-        FileUtils.copyFileToDirectory(file, new File("C:\\"));
+    public void generateQrcode() {
+        QrCodeUtils.text("ni收到试试试试").withSize(600, 600).writeToFile(new File("C:\\1234.png"));
+        System.out.println("解码:" + QrCodeUtils.decodeQrcode(new File("C:\\1234.png")));
     }
 
 }
