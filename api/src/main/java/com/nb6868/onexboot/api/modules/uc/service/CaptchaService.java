2530950bdc276609a1546d39a6b181d48be7043f
Charles <zhangchaoxu@gmail.com>
2021-01-27 18:06:28 +0800

    change captcha method name

--- api/src/main/java/com/nb6868/onexboot/api/modules/uc/service/CaptchaService.java
+++ api/src/main/java/com/nb6868/onexboot/api/modules/uc/service/CaptchaService.java
@@ -11,19 +11,21 @@ public interface CaptchaService {
 
     /**
      * 生成图片验证码
+     *
      * @param uuid uuid
      * @param width 宽度
      * @param height 高度
      * @param type 类型
-     * @return 生成的图片base64
+     * @return 生成的图片
      */
     Captcha createCaptcha(String uuid, int width, int height, String type);
 
     /**
-     * 效验验证码
+     * 校验验证码
+     *
      * @param uuid  uuid
-     * @param code  验证码
-     * @return  true：成功  false：失败
+     * @param code  验证码内容
+     * @return  验证结果
      */
     boolean validate(String uuid, String code);
 }
