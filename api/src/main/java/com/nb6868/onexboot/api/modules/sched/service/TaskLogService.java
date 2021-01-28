ded374136be55bb99dcc963727d8ac069b064509
Charles <zhangchaoxu@gmail.com>
2021-01-28 16:06:19 +0800

    change task query

--- api/src/main/java/com/nb6868/onexboot/api/modules/sched/service/TaskLogService.java
+++ api/src/main/java/com/nb6868/onexboot/api/modules/sched/service/TaskLogService.java
@@ -1,20 +1,14 @@
 package com.nb6868.onexboot.api.modules.sched.service;
 
 import com.nb6868.onexboot.api.modules.sched.dto.TaskLogDTO;
-import com.nb6868.onexboot.common.pojo.PageData;
-import com.nb6868.onexboot.common.service.BaseService;
 import com.nb6868.onexboot.api.modules.sched.entity.TaskLogEntity;
-
-import java.util.Map;
+import com.nb6868.onexboot.common.service.CrudService;
 
 /**
  * 定时任务日志
  *
  * @author Charles zhangchaoxu@gmail.com
  */
-public interface TaskLogService extends BaseService<TaskLogEntity> {
+public interface TaskLogService extends CrudService<TaskLogEntity, TaskLogDTO> {
 
-	PageData<TaskLogDTO> page(Map<String, Object> params);
-
-	TaskLogDTO get(Long id);
 }
