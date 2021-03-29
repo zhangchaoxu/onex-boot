7d30032e67685016497871c0337db273fe7107e9
Charles <zhangchaoxu@gmail.com>
2021-03-29 11:43:40 +0800

    add onex props

--- api/src/main/java/com/nb6868/onexboot/api/common/config/OnexProps.java
+++ api/src/main/java/com/nb6868/onexboot/api/common/config/OnexProps.java
@@ -7,6 +7,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
 /**
  * OneX相关配置文件
  *
@@ -17,42 +23,71 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "onex")
 @PropertySource(value = "classpath:onex.yml", factory = YamlPropertySourceFactory.class)
-public class OnexProperties {
+public class OnexProps implements Serializable {
 
     /**
      * 登录配置源,支持配置文件和数据库
      */
-    private LoginConfigSource loginConfigSource = LoginConfigSource.PROPS;
+    private LoginPropsSource loginPropsSource = LoginPropsSource.PROPS;
 
     /**
-     * 后台登录
+     * 登录配置
      */
-    private final LoginProperties loginAdminConfig = new LoginProperties();
+    private final LoginProps loginProps = new LoginProps();
 
     @Data
     @NoArgsConstructor
-    public static class LoginProperties {
+    public static class LoginProps implements Serializable {
 
+        /**
+         * 登录类型
+         */
+        private String type;
         /**
          * 是否支持多端登录
          * 支持多端登录,表示可以在不同客户端登录,创建token的时候不判断原先的
          * 不支持多端登录,表示同一个帐号只能在一个地方登录,创建token的时候会将原先的token删除
          */
+        @JsonIgnore
+        @ApiModelProperty(value = "支持多客户端登录")
         private boolean multiLogin = false;
         /**
          * token策略,jwt或者uuid
          */
+        @JsonIgnore
+        @ApiModelProperty(value = "支持多客户端登录")
         private TokenPolicy tokenPolicy = TokenPolicy.UUID;
         /**
          * token有效时间
          */
+        @JsonIgnore
+        @ApiModelProperty(value = "支持多客户端登录")
         private Long tokenExpire = 604800L;
         /**
          * token是否自动延期
          */
+        @JsonIgnore
+        @ApiModelProperty(value = "支持多客户端登录")
         private boolean tokenRenewal = true;
-        private boolean roleBase = true;
+        /**
+         * 基于角色控制
+         */
+        private boolean roleBase = false;
+        /**
+         * 基于权限控制
+         */
         private boolean permissionBase = true;
+        /**
+         * 是否有验证码
+         */
+        @ApiModelProperty(value = "是否需要验证码")
+        private boolean captcha = false;
+        /**
+         * 魔术验证码
+         */
+        @JsonIgnore
+        @ApiModelProperty(value = "魔术验证码")
+        private String magicCaptcha = "";
 
     }
 }
