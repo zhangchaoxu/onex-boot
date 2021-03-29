2bba121d7bdfd5e3ad41c42608552dbd9c26427c
Charles <zhangchaoxu@gmail.com>
2021-03-29 16:59:31 +0800

    change shiro service to auth service

--- api/src/main/java/com/nb6868/onexboot/api/modules/uc/service/AuthService.java
+++ api/src/main/java/com/nb6868/onexboot/api/modules/uc/service/AuthService.java
@@ -1,21 +1,36 @@
 package com.nb6868.onexboot.api.modules.uc.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nb6868.onexboot.api.common.config.LoginProps;
 import com.nb6868.onexboot.api.common.config.LoginPropsSource;
 import com.nb6868.onexboot.api.common.config.OnexProps;
+import com.nb6868.onexboot.api.modules.msg.MsgConst;
+import com.nb6868.onexboot.api.modules.msg.entity.MailLogEntity;
+import com.nb6868.onexboot.api.modules.msg.service.MailLogService;
 import com.nb6868.onexboot.api.modules.sys.service.ParamService;
 import com.nb6868.onexboot.api.modules.uc.UcConst;
 import com.nb6868.onexboot.api.modules.uc.dingtalk.DingtalkScanProps;
+import com.nb6868.onexboot.api.modules.uc.dto.LoginRequest;
 import com.nb6868.onexboot.api.modules.uc.entity.MenuEntity;
 import com.nb6868.onexboot.api.modules.uc.entity.TokenEntity;
 import com.nb6868.onexboot.api.modules.uc.entity.UserEntity;
+import com.nb6868.onexboot.api.modules.uc.entity.UserOauthEntity;
 import com.nb6868.onexboot.api.modules.uc.user.UserDetail;
 import com.nb6868.onexboot.api.modules.uc.wx.WxScanProps;
+import com.nb6868.onexboot.common.exception.ErrorCode;
+import com.nb6868.onexboot.common.exception.OnexException;
+import com.nb6868.onexboot.common.pojo.Kv;
+import com.nb6868.onexboot.common.util.JacksonUtils;
+import com.nb6868.onexboot.common.util.PasswordUtils;
 import com.nb6868.onexboot.common.util.StringUtils;
+import com.nb6868.onexboot.common.validator.AssertUtils;
+import com.nb6868.onexboot.common.validator.ValidatorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,6 +50,8 @@ public class AuthService {
     @Autowired
     private UserService userService;
     @Autowired
+    private UserOauthService userOauthService;
+    @Autowired
     private RoleService roleService;
     @Autowired
     private TokenService tokenService;
@@ -42,6 +59,10 @@ public class AuthService {
     private ParamService paramService;
     @Autowired
     private MenuScopeService menuScopeService;
+    @Autowired
+    private CaptchaService captchaService;
+    @Autowired
+    private MailLogService mailLogService;
 
     /**
      * 获取用户权限列表
@@ -167,6 +188,126 @@ public class AuthService {
             loginAdminProps.setDingtalkScanProps(paramService.getContentObject(UcConst.LOGIN_TYPE_PREFIX + UcConst.LoginTypeEnum.ADMIN_DINGTALK_SCAN.name() + "_CONFIG", DingtalkScanProps.class));
         }
         return loginAdminProps;
+    }
+
+    public Kv login(LoginRequest loginRequest) {
+        // 获得登录配置
+        LoginProps loginProps = getLoginProps(UcConst.LOGIN_TYPE_PREFIX + loginRequest.getType());
+        AssertUtils.isNull(loginProps, ErrorCode.UNKNOWN_LOGIN_TYPE);
+
+        // 校验验证码
+        if (loginProps.isCaptcha()) {
+            ValidatorUtils.validateEntity(loginRequest, LoginRequest.CaptchaGroup.class);
+            boolean validateCaptcha = loginRequest.getCaptcha().equalsIgnoreCase(loginProps.getMagicCaptcha()) || captchaService.validate(loginRequest.getUuid(), loginRequest.getCaptcha());
+            AssertUtils.isFalse(validateCaptcha, ErrorCode.CAPTCHA_ERROR);
+        }
+
+        // 登录用户
+        UserEntity user;
+        if (UcConst.LoginTypeEnum.ADMIN_USERNAME_PASSWORD.name().equalsIgnoreCase(loginRequest.getType()) || UcConst.LoginTypeEnum.APP_USER_PWD.name().equalsIgnoreCase(loginRequest.getType())) {
+            // 帐号密码登录
+            ValidatorUtils.validateEntity(loginRequest, LoginRequest.UsernamePasswordGroup.class);
+            user = userService.getOneByColumn("username", loginRequest.getUsername());
+            if (user == null) {
+                // 帐号不存在
+                throw new OnexException(ErrorCode.ACCOUNT_NOT_EXIST);
+            } else if (user.getState() != UcConst.UserStateEnum.ENABLED.value()) {
+                // 帐号锁定
+                throw new OnexException(ErrorCode.ACCOUNT_DISABLE);
+            } else if (!PasswordUtils.matches(loginRequest.getPassword(), user.getPassword())) {
+                // 密码不匹配
+                throw new OnexException(ErrorCode.ACCOUNT_PASSWORD_ERROR);
+            }
+        }  else if (UcConst.LoginTypeEnum.ADMIN_MOBILE_SMSCODE.name().equalsIgnoreCase(loginRequest.getType()) || UcConst.LoginTypeEnum.APP_MOBILE_SMS.name().equalsIgnoreCase(loginRequest.getType())) {
+            // 手机号验证码登录
+            ValidatorUtils.validateEntity(loginRequest, LoginRequest.MobileSmsCodeGroup.class);
+            user = userService.getOneByColumn("mobile", loginRequest.getMobile());
+            if (user == null) {
+                // 帐号不存在
+                throw new OnexException(ErrorCode.ACCOUNT_NOT_EXIST);
+            } else if (user.getState() != UcConst.UserStateEnum.ENABLED.value()) {
+                // 帐号锁定
+                throw new OnexException(ErrorCode.ACCOUNT_DISABLE);
+            }
+            //  校验验证码
+            MailLogEntity lastSmsLog = mailLogService.findLastLogByTplCode(MsgConst.SMS_TPL_LOGIN, loginRequest.getMobile());
+            if (null == lastSmsLog || !loginRequest.getSmsCode().equalsIgnoreCase(JacksonUtils.jsonToMap(lastSmsLog.getContentParams()).get("code").toString())) {
+                // 验证码错误,找不到验证码
+                throw new OnexException(ErrorCode.SMS_CODE_ERROR);
+            } else {
+                // 验证码正确
+                // 校验过期时间
+                if (lastSmsLog.getValidEndTime() != null && lastSmsLog.getValidEndTime().before(new Date())) {
+                    throw new OnexException(ErrorCode.SMS_CODE_EXPIRED);
+                }
+                // 将短信消费掉
+                mailLogService.consumeById(lastSmsLog.getId());
+            }
+        } else if (UcConst.LoginTypeEnum.APP_APPLE.name().equalsIgnoreCase(loginRequest.getType())) {
+            // 苹果登录
+            ValidatorUtils.validateEntity(loginRequest, LoginRequest.AppleGroup.class);
+            // jwt解析identityToken, 获取userIdentifier
+            DecodedJWT jwt = JWT.decode(loginRequest.getAppleIdentityToken());
+            // app包名
+            String packageName = jwt.getAudience().get(0);
+            // 用户id
+            String userIdentifier = jwt.getSubject();
+            // 有效期
+            Date expireTime = jwt.getExpiresAt();
+            if (expireTime.after(new Date())) {
+                throw new OnexException(ErrorCode.APPLE_LOGIN_ERROR);
+            } else {
+                // todo 使用apple keys做验证
+                // {https://developer.apple.com/cn/app-store/review/guidelines/#sign-in-with-apple}
+                // 通过packageName和userIdentifier找对应的数据记录
+                UserOauthEntity userApple = userOauthService.getByOpenid(userIdentifier);
+                if (userApple == null) {
+                    // 不存在记录,则保存记录
+                    userApple = new UserOauthEntity();
+                    userApple.setAppid(packageName);
+                    userApple.setOpenid(userIdentifier);
+                    userApple.setType(UcConst.OauthTypeEnum.APPLE.name());
+                    userOauthService.save(userApple);
+                }
+                if (userApple.getUserId() == null) {
+                    // 未绑定用户
+                    throw new OnexException(ErrorCode.APPLE_NOT_BIND);
+                } else {
+                    user = userService.getById(userApple.getUserId());
+                    if (user == null) {
+                        // 帐号不存在
+                        throw new OnexException(ErrorCode.ACCOUNT_NOT_EXIST);
+                    } else if (user.getState() != UcConst.UserStateEnum.ENABLED.value()) {
+                        // 帐号锁定
+                        throw new OnexException(ErrorCode.ACCOUNT_DISABLE);
+                    }
+                }
+            }
+        } else {
+            throw new OnexException(ErrorCode.UNKNOWN_LOGIN_TYPE);
+        }
+
+        /*if (user == null && loginChannelCfg.isAutoCreate()) {
+            // 没有该用户，并且需要自动创建用户
+            user = new UserDTO();
+            user.setState(UcConst.UserStateEnum.ENABLED.value());
+            user.setMobile(loginRequest.getMobile());
+            user.setUsername(loginRequest.getMobile());
+            user.setType(UcConst.UserTypeEnum.USER.value());
+            user.setGender(3);
+            // 密码加密
+            user.setPassword(PasswordUtils.encode(loginRequest.getMobile()));
+            saveDto(user);
+            //保存角色用户关系
+            roleUserService.saveOrUpdate(user.getId(), user.getRoleIdList());
+        }*/
+
+        // 登录成功
+        Kv kv = Kv.init();
+        kv.set(UcConst.TOKEN_HEADER, tokenService.createToken(user.getId(), loginProps));
+        kv.set("expire", loginProps.getTokenExpire());
+        kv.set("user", user);
+        return kv;
     }
 
 }
--- api/src/main/java/com/nb6868/onexboot/api/modules/uc/shiro/ShiroRealm.java
+++ api/src/main/java/com/nb6868/onexboot/api/modules/uc/shiro/ShiroRealm.java
@@ -30,7 +30,7 @@ import java.util.Set;
 public class ShiroRealm extends AuthorizingRealm {
 
     @Autowired
-    private AuthService shiroService;
+    private AuthService authService;
 
     /**
      * 必须重写此方法，不然Shiro会报错
@@ -55,14 +55,14 @@ public class ShiroRealm extends AuthorizingRealm {
             return new SimpleAuthenticationInfo(userDetail, UcConst.TOKEN_ANON, getName());
         }
         // 根据accessToken，查询用户信息
-        TokenEntity token = shiroService.getUserIdAndTypeByToken(accessToken);
+        TokenEntity token = authService.getUserIdAndTypeByToken(accessToken);
         // token失效
         if (token == null) {
             throw new IncorrectCredentialsException(MessageUtils.getMessage(ErrorCode.TOKEN_INVALID));
         }
 
         // 查询用户信息
-        UserEntity userEntity = shiroService.getUser(token.getUserId());
+        UserEntity userEntity = authService.getUser(token.getUserId());
 
         if (userEntity == null) {
             // 账号不存在
@@ -76,14 +76,14 @@ public class ShiroRealm extends AuthorizingRealm {
         UserDetail userDetail = ConvertUtils.sourceToTarget(userEntity, UserDetail.class);
 
         // 获取用户对应的部门数据权限
-        /*List<Long> deptIdList = shiroService.getDataScopeList(userDetail.getId());
+        /*List<Long> deptIdList = authService.getDataScopeList(userDetail.getId());
         userDetail.setDeptIdList(deptIdList);*/
 
-        LoginProps loginProps = shiroService.getLoginProps(token.getType());
+        LoginProps loginProps = authService.getLoginProps(token.getType());
         userDetail.setLoginProps(loginProps);
         if (loginProps.isTokenRenewal()) {
             // 更新token
-            shiroService.renewalToken(accessToken, loginProps.getTokenExpire());
+            authService.renewalToken(accessToken, loginProps.getTokenExpire());
         }
 
         return new SimpleAuthenticationInfo(userDetail, accessToken, getName());
@@ -106,11 +106,11 @@ public class ShiroRealm extends AuthorizingRealm {
             // 根据配置中的role和permission设置SimpleAuthorizationInfo
             if (null != user.getLoginProps() && user.getLoginProps().isRoleBase()) {
                 // 塞入角色列表
-                info.setRoles(shiroService.getUserRoles(user));
+                info.setRoles(authService.getUserRoles(user));
             }
             if (null != user.getLoginProps() && user.getLoginProps().isPermissionBase()) {
                 // 塞入权限列表
-                info.setStringPermissions(shiroService.getUserPermissions(user));
+                info.setStringPermissions(authService.getUserPermissions(user));
             }
         }
         return info;
