1919e3f4678eda6d834e5b4ae163f6a283998baa
Charles <zhangchaoxu@gmail.com>
2021-04-07 09:19:52 +0800

    change msg send

--- api/src/main/java/com/nb6868/onexboot/api/modules/msg/controller/MailLogController.java
+++ api/src/main/java/com/nb6868/onexboot/api/modules/msg/controller/MailLogController.java
@@ -4,16 +4,10 @@ import com.nb6868.onexboot.api.common.annotation.LogOperation;
 import com.nb6868.onexboot.api.modules.msg.MsgConst;
 import com.nb6868.onexboot.api.modules.msg.dto.MailLogDTO;
 import com.nb6868.onexboot.api.modules.msg.dto.MailSendRequest;
-import com.nb6868.onexboot.api.modules.msg.entity.MailLogEntity;
-import com.nb6868.onexboot.api.modules.msg.entity.MailTplEntity;
 import com.nb6868.onexboot.api.modules.msg.service.MailLogService;
 import com.nb6868.onexboot.api.modules.msg.service.MailTplService;
-import com.nb6868.onexboot.common.pojo.Kv;
 import com.nb6868.onexboot.common.pojo.PageData;
 import com.nb6868.onexboot.common.pojo.Result;
-import com.nb6868.onexboot.common.util.DateUtils;
-import com.nb6868.onexboot.common.util.JacksonUtils;
-import com.nb6868.onexboot.common.util.StringUtils;
 import com.nb6868.onexboot.common.validator.AssertUtils;
 import com.nb6868.onexboot.common.validator.group.AddGroup;
 import io.swagger.annotations.Api;
@@ -93,20 +87,6 @@ public class MailLogController {
     public Result<?> sendCode(@Validated(value = {AddGroup.class}) @RequestBody MailSendRequest dto) {
         // 只允许发送CODE_开头的模板
         AssertUtils.isFalse(dto.getTplCode().startsWith(MsgConst.SMS_CODE_TPL_PREFIX), "只支持" + MsgConst.SMS_CODE_TPL_PREFIX + "类型模板发送");
-        // 消息模板
-        MailTplEntity mailTpl = mailTplService.getOneByColumn("code", dto.getTplCode());
-        AssertUtils.isNull(mailTpl, "找不到对应的消息模板:" + dto.getTplCode());
-        if (mailTpl.getTimeLimit() > 0) {
-            // 有时间限制
-            // 先校验该收件人是否timeLimit秒内发送过
-            MailLogEntity lastMailLog = mailLogService.findLastLogByTplCode(dto.getTplCode(), dto.getMailTo());
-            if (null != lastMailLog && DateUtils.timeDiff(lastMailLog.getCreateTime()) < mailTpl.getTimeLimit() * 1000) {
-                // 限定时间内已经发送过了
-                return new Result<>().error("发送请求过于频繁");
-            }
-        }
-        dto.setContentParam(JacksonUtils.pojoToJson(Kv.init().set("code", StringUtils.getRandomDec(4))));
-
         boolean flag = mailLogService.send(dto);
         if (flag) {
             return new Result<>();
