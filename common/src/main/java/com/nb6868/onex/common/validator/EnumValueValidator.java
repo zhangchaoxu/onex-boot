c3f6af33a08cbd53970602751266b25a29735eb4
Charles <zhangchaoxu@gmail.com>
2021-08-26 09:09:45 +0800

    add enumvalue check

---
+++ common/src/main/java/com/nb6868/onex/common/validator/EnumValueValidator.java
@@ -0,0 +1,40 @@
+package com.nb6868.onex.common.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * EnumValue校验方法
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+public class EnumValueValidator implements ConstraintValidator<EnumValue, Object> {
+
+    private String[] strValues;
+    private int[] intValues;
+
+    @Override
+    public void initialize(EnumValue constraintAnnotation) {
+        strValues = constraintAnnotation.strValues();
+        intValues = constraintAnnotation.intValues();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (value instanceof String) {
+            for (String s : strValues) {
+                if (s.equals(value)) {
+                    return true;
+                }
+            }
+        } else if (value instanceof Integer) {
+            for (int s : intValues) {
+                if (s == (Integer) value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
