7f886f417a3379336763003d230c8155c84addfa
Charles <zhangchaoxu@gmail.com>
2021-08-31 18:06:57 +0800

    add login props

---
+++ common/src/main/java/com/nb6868/onex/common/auth/LoginProps.java
@@ -0,0 +1,90 @@
+package com.nb6868.onex.common.auth;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nb6868.onex.common.config.YamlPropertySourceFactory;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 登录配置
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "login")
+@PropertySource(value = "classpath:onex.yml", factory = YamlPropertySourceFactory.class)
+public class LoginProps {
+
+
+    @ApiModelProperty(value = "配置项")
+    private Map<String, Config> configs;
+
+    @Data
+    public static class Config {
+
+        @ApiModelProperty(value = "登录类型")
+        private String type;
+
+        /**
+         * 支持多端登录,表示可以在不同客户端登录,创建token的时候不判断原先的
+         * 不支持多端登录,表示同一个帐号只能在一个地方登录,创建token的时候会将原先的token删除
+         */
+        @JsonIgnore
+        @ApiModelProperty(value = "支持多客户端登录")
+        private boolean multiLogin = false;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "token策略")
+        private String tokenPolicy = "uuid";
+
+        @JsonIgnore
+        @ApiModelProperty(value = "token有效时间")
+        private Long tokenExpire = 604800L;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "token是否自动延期")
+        private boolean tokenRenewal = true;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "基于角色控制")
+        private boolean roleBase = false;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "基于权限控制")
+        private boolean permissionBase = true;
+
+        @ApiModelProperty(value = "是否需要验证码")
+        private boolean captcha = false;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "魔术验证码")
+        private String magicCaptcha;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "最多登录次数")
+        private Integer tryTimesMax;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "超过最大登录次数后锁定时间")
+        private Long loginErrorLockTime;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "自动创建用户")
+        private boolean autoCreate = false;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "自动创建用户的角色ids")
+        private String autoCreateUserRoleIds;
+
+        @JsonIgnore
+        @ApiModelProperty(value = "首次登录强制修改密码")
+        private Boolean forceUpdatePassword;
+    }
+
+}
