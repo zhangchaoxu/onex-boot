e156ea1665196dcfd4ad430e2fe1570141a68193
Charles <zhangchaoxu@gmail.com>
2021-09-18 13:33:29 +0800

    fix shiro issue

---
+++ common/src/main/java/com/nb6868/onex/common/filter/BaseShiroFilter.java
@@ -0,0 +1,86 @@
+package com.nb6868.onex.common.filter;
+
+import com.nb6868.onex.common.exception.ErrorCode;
+import com.nb6868.onex.common.pojo.Result;
+import com.nb6868.onex.common.util.JacksonUtils;
+import lombok.SneakyThrows;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 基础shiro过滤器
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+public abstract class BaseShiroFilter extends AuthenticatingFilter {
+
+    @Override
+    protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
+        AuthenticationToken token = createToken(request, response);
+        if (token == null) {
+            return onLoginFailure(token, null, request, response);
+        }
+        try {
+            Subject subject = getSubject(request, response);
+            subject.login(token);
+            return onLoginSuccess(token, subject, request, response);
+        } catch (AuthenticationException e) {
+            return onLoginFailure(token, e, request, response);
+        }
+    }
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        // 放行所有OPTIONS请求
+        return RequestMethod.OPTIONS.name().equals(((HttpServletRequest) request).getMethod());
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        return executeLogin(request, response);
+    }
+
+    /**
+     * 登录失败
+     *
+     * Realm.doGetAuthenticationInfo抛出的异常会在这里捕获处理
+     */
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+        responseUnauthorized(request, response, e);
+        return false;
+    }
+
+    /**
+     * 响应未授权
+     */
+    @SneakyThrows
+    @SuppressWarnings("deprecation")
+    protected void responseUnauthorized(ServletRequest request, ServletResponse response, Exception e) {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        httpResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        httpResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ((HttpServletRequest) request).getHeader(HttpHeaders.ORIGIN));
+
+        // 处理登录失败的异常
+        if (e == null) {
+            String json = JacksonUtils.pojoToJson(new Result<>().error(ErrorCode.UNAUTHORIZED));
+            httpResponse.getWriter().print(json);
+        } else {
+            Throwable throwable = e.getCause() == null ? e : e.getCause();
+            String json = JacksonUtils.pojoToJson(new Result<>().error(ErrorCode.UNAUTHORIZED), throwable.getMessage());
+            httpResponse.getWriter().print(json);
+        }
+    }
+
+}
--- common/src/main/java/com/nb6868/onex/common/filter/JwtShiroFilter.java
+++ common/src/main/java/com/nb6868/onex/common/filter/JwtShiroFilter.java
@@ -4,24 +4,13 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.jwt.JWT;
 import com.nb6868.onex.common.auth.AuthProps;
-import com.nb6868.onex.common.exception.ErrorCode;
-import com.nb6868.onex.common.pojo.Result;
 import com.nb6868.onex.common.util.HttpContextUtils;
-import com.nb6868.onex.common.util.JacksonUtils;
 import com.nb6868.onex.common.util.JwtUtils;
-import lombok.SneakyThrows;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * JWT Shiro过滤器
@@ -29,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Charles zhangchaoxu@gmail.com
  */
-public class JwtShiroFilter extends AuthenticatingFilter {
+public class JwtShiroFilter extends BaseShiroFilter {
 
     private final AuthProps authProps;
 
@@ -39,7 +28,6 @@ public class JwtShiroFilter extends AuthenticatingFilter {
 
     @Override
     protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
-        // 请求token
         String token = HttpContextUtils.getRequestParameter((HttpServletRequest) request, authProps.getTokenKey());
         if (StrUtil.isNotBlank(token)) {
             // 验证token
@@ -67,50 +55,6 @@ public class JwtShiroFilter extends AuthenticatingFilter {
             }
         }
         return null;
-    }
-
-    @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
-        // 放行所有OPTIONS请求
-        return RequestMethod.OPTIONS.name().equals(((HttpServletRequest) request).getMethod());
-    }
-
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        return executeLogin(request, response);
-    }
-
-    /**
-     * 登录失败
-     *
-     * Realm.doGetAuthenticationInfo抛出的异常会在这里捕获处理
-     */
-    @Override
-    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
-        responseUnauthorized(request, response, e);
-        return false;
-    }
-
-    /**
-     * 响应未授权
-     */
-    @SneakyThrows
-    @SuppressWarnings("deprecation")
-    protected void responseUnauthorized(ServletRequest request, ServletResponse response, Exception e) {
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        httpResponse.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        httpResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        httpResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ((HttpServletRequest) request).getHeader(HttpHeaders.ORIGIN));
-
-        // 处理登录失败的异常
-        if (e == null) {
-            String json = JacksonUtils.pojoToJson(new Result<>().error(ErrorCode.UNAUTHORIZED));
-            httpResponse.getWriter().print(json);
-        } else {
-            Throwable throwable = e.getCause() == null ? e : e.getCause();
-            String json = JacksonUtils.pojoToJson(new Result<>().error(ErrorCode.UNAUTHORIZED), throwable.getMessage());
-            httpResponse.getWriter().print(json);
-        }
     }
 
 }
