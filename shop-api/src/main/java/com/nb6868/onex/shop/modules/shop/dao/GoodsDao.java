368c5967d1cd89eb7e186544f77d96e402200452
Charles <zhangchaoxu@gmail.com>
2021-09-22 16:22:28 +0800

    add api order

---
+++ shop-api/src/main/java/com/nb6868/onex/shop/modules/shop/dao/GoodsDao.java
@@ -0,0 +1,15 @@
+package com.nb6868.onex.shop.modules.shop.dao;
+
+import com.nb6868.onex.common.jpa.BaseDao;
+import com.nb6868.onex.shop.modules.shop.entity.GoodsEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+@Mapper
+public interface GoodsDao extends BaseDao<GoodsEntity> {
+
+}
