76da61051aaac14d1e400adbc693d036e43cfcc3
Charles <zhangchaoxu@gmail.com>
2021-09-22 15:57:20 +0800

    add cart into shop

---
+++ shop-api/src/main/java/com/nb6868/onex/shop/modules/shop/dao/ReceiverDao.java
@@ -0,0 +1,15 @@
+package com.nb6868.onex.shop.modules.shop.dao;
+
+import com.nb6868.onex.common.jpa.BaseDao;
+import com.nb6868.onex.shop.modules.shop.entity.ReceiverEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 收件地址
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+@Mapper
+public interface ReceiverDao extends BaseDao<ReceiverEntity> {
+
+}
