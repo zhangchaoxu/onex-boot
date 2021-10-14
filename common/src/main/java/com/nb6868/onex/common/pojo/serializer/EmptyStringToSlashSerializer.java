9a618a726442a0705657981aafbed9de86fdd7b0
Charles <zhangchaoxu@gmail.com>
2021-10-14 19:14:03 +0800

    add serialize

---
+++ common/src/main/java/com/nb6868/onex/common/pojo/serializer/EmptyStringToSlashSerializer.java
@@ -0,0 +1,25 @@
+package com.nb6868.onex.common.pojo.serializer;
+
+import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * 空字符串转成斜杠
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+public class EmptyStringToSlashSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (StrUtil.isBlank(value)) {
+            gen.writeString("/");
+        } else {
+            gen.writeString(value);
+        }
+    }
+}
