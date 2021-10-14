059078a9b2e8a8c20a5a9fe7b9e4998c305294d1
Charles <zhangchaoxu@gmail.com>
2021-10-14 17:20:13 +0800

    加入table data

---
+++ common/src/main/java/com/nb6868/onex/common/pojo/TableData.java
@@ -0,0 +1,37 @@
+package com.nb6868.onex.common.pojo;
+
+import cn.hutool.core.lang.Dict;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 表格数据
+ * @author Charles zhangchoaxu@gmail.com
+ */
+@Data
+public class TableData<T> {
+
+    @ApiModelProperty(value = "表名称")
+    private String title;
+
+    @ApiModelProperty(value = "导出地址")
+    private String exportUrl;
+
+    @ApiModelProperty(value = "是否支持导出")
+    private boolean export = false;
+
+    @ApiModelProperty(value = "是否分页")
+    private boolean page = true;
+
+    @ApiModelProperty(value = "列表/分页数据")
+    private PageData<T> data;
+
+    @ApiModelProperty(value = "表头定义")
+    private List<Dict> schemas;
+
+    @ApiModelProperty(value = "额外数据")
+    private Dict ext;
+
+}
---
+++ common/src/main/java/com/nb6868/onex/common/pojo/TableSchema.java
@@ -0,0 +1,37 @@
+package com.nb6868.onex.common.pojo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.apache.poi.ss.formula.functions.T;
+
+/**
+ * 表格头定义
+ * see https://element.eleme.cn/#/zh-CN/component/table
+ *
+ * @author Charles zhangchoaxu@gmail.com
+ */
+@Data
+public class TableSchema {
+
+    @ApiModelProperty(value = "列类型,selection/index/expand")
+    private String type;
+
+    @ApiModelProperty(value = "显示的标题")
+    private String label;
+
+    @ApiModelProperty(value = "列内容字段名")
+    private String prop;
+
+    @ApiModelProperty(value = "链接对应字段名")
+    private String linkProp;
+
+    @ApiModelProperty(value = "列宽度")
+    private String width;
+
+    @ApiModelProperty(value = "列最小宽度")
+    private String minWidth;
+
+    @ApiModelProperty(value = "当内容过长被隐藏时显示 tooltip")
+    private String showOverflowTooltip;
+
+}
