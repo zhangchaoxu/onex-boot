db3b4b2024ed573e4bb600d6d4713066cddbbe70
Charles <zhangchaoxu@gmail.com>
2021-10-21 11:25:50 +0800

    upgrade libs

---
+++ common/src/main/java/com/nb6868/onex/common/pojo/UsernamePasswordLoginRequest.java
@@ -0,0 +1,24 @@
+package com.nb6868.onex.common.pojo;
+
+import com.nb6868.onex.common.validator.group.DefaultGroup;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "用户名密码登录请求")
+public class UsernamePasswordLoginRequest {
+
+    @ApiModelProperty(value = "用户名")
+    @NotEmpty(message = "用户名不能为空", groups = {DefaultGroup.class})
+    private String username;
+
+    @ApiModelProperty(value = "密码")
+    @NotEmpty(message = "密码不能为空", groups = {DefaultGroup.class})
+    private String password;
+
+}
