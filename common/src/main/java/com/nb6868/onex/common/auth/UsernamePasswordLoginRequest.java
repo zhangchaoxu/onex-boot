9404405904b6862e49ce7d235fe627dca34c57fa
Charles <zhangchaoxu@gmail.com>
2021-11-03 15:53:54 +0800

    release 1.5.62

--- common/src/main/java/com/nb6868/onex/common/auth/UsernamePasswordLoginRequest.java
+++ common/src/main/java/com/nb6868/onex/common/auth/UsernamePasswordLoginRequest.java
@@ -1,4 +1,4 @@
-package com.nb6868.onex.common.pojo;
+package com.nb6868.onex.common.auth;
 
 import com.nb6868.onex.common.validator.group.DefaultGroup;
 import io.swagger.annotations.ApiModel;
@@ -7,11 +7,16 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @ApiModel(value = "用户名密码登录请求")
-public class UsernamePasswordLoginRequest {
+public class UsernamePasswordLoginRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "登录配置编码", example = "ADMIN_USERNAME_PASSWORD")
+    private String authConfigKey = "ADMIN_USERNAME_PASSWORD";
 
     @ApiModelProperty(value = "用户名", required = true)
     @NotEmpty(message = "用户名不能为空", groups = {DefaultGroup.class})
