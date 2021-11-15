feafefffa3a79c4fb821e70de45fce6bb191eabe
Charles <zhangchaoxu@gmail.com>
2021-11-15 11:27:52 +0800

    add hw sms service

---
+++ msg/src/main/java/com/nb6868/onex/msg/mail/RobotDingtalkMailService.java
@@ -0,0 +1,37 @@
+package com.nb6868.onex.msg.mail;
+
+import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.nb6868.onex.common.dingtalk.BaseResponse;
+import com.nb6868.onex.common.dingtalk.DingTalkApi;
+import com.nb6868.onex.msg.dto.MailSendRequest;
+import com.nb6868.onex.msg.entity.MailTplEntity;
+
+/**
+ * 钉钉机器人消息
+ * see {https://developers.dingtalk.com/document/robots/custom-robot-access}
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+public class RobotDingtalkMailService extends AbstractMailService {
+
+    @Override
+    public boolean sendMail(MailTplEntity mailTpl, MailSendRequest request) {
+        JSONObject tplParam = JSONUtil.parseObj(mailTpl.getParam());
+        JSONObject sendParam = JSONUtil.parseObj(request.getContentParam());
+        String msgtype =  sendParam.getStr("msgtype");
+        String keywords =  tplParam.getStr("keywords");
+        if ("text".equalsIgnoreCase(msgtype)) {
+            // text类型消息,要求有关键词,但实际未包含,补充上
+            if (StrUtil.isNotBlank(keywords) && !StrUtil.contains(sendParam.getJSONObject("text").getStr("content"), keywords)) {
+                sendParam.set("text", new JSONObject().set("content", keywords + "\n" + sendParam.getJSONObject("text").getStr("content")));
+            }
+        }
+        // https://oapi.dingtalk.com/robot/send?access_token=xxxx
+        BaseResponse sendResponse = DingTalkApi.sendRobotMsg(tplParam.getStr("access_token"), JSONUtil.toBean(sendParam, Dict.class));
+        return sendResponse.isSuccess();
+    }
+
+}
--- msg/src/main/java/com/nb6868/onex/msg/mail/SmsHwcloudMailService.java
+++ msg/src/main/java/com/nb6868/onex/msg/mail/SmsHwcloudMailService.java
@@ -1,18 +1,265 @@
 package com.nb6868.onex.msg.mail;
 
+import cn.hutool.core.lang.UUID;
+import com.nb6868.onex.common.exception.ErrorCode;
+import com.nb6868.onex.common.pojo.Const;
+import com.nb6868.onex.common.util.JacksonUtils;
+import com.nb6868.onex.common.util.SpringContextUtils;
+import com.nb6868.onex.common.validator.AssertUtils;
 import com.nb6868.onex.msg.dto.MailSendRequest;
+import com.nb6868.onex.msg.entity.MailLogEntity;
 import com.nb6868.onex.msg.entity.MailTplEntity;
+import com.nb6868.onex.msg.mail.sms.SmsProps;
+import com.nb6868.onex.msg.service.MailLogService;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 短信 华为云 消息服务
+ * see {https://support.huaweicloud.com/devg-msgsms/sms_04_0002.html}
  *
  * @author Charles zhangchaoxu@gmail.com
  */
+@Slf4j
 public class SmsHwcloudMailService extends AbstractMailService {
+
+    // 无需修改,用于格式化鉴权头域,给"X-WSSE"参数赋值
+    private static final String WSSE_HEADER_FORMAT = "UsernameToken Username=\"%s\",PasswordDigest=\"%s\",Nonce=\"%s\",Created=\"%s\"";
+    // 无需修改,用于格式化鉴权头域,给"Authorization"参数赋值
+    private static final String AUTH_HEADER_VALUE = "WSSE realm=\"SDP\",profile=\"UsernameToken\",type=\"Appkey\"";
 
     @Override
     public boolean sendMail(MailTplEntity mailTpl, MailSendRequest request) {
-        return false;
+        SmsProps smsProps = JacksonUtils.jsonToPojo(mailTpl.getParam(), SmsProps.class);
+        AssertUtils.isNull(smsProps, ErrorCode.PARAM_CFG_ERROR);
+
+        MailLogService mailLogService = SpringContextUtils.getBean(MailLogService.class);
+
+        // 发送记录记录
+        MailLogEntity mailLog = new MailLogEntity();
+        mailLog.setMailTo(request.getMailTo());
+        mailLog.setContent(mailTpl.getContent());
+        mailLog.setTplCode(mailTpl.getCode());
+        mailLog.setTplType(mailTpl.getType());
+        mailLog.setContentParams(request.getContentParam());
+        mailLog.setConsumeState(Const.BooleanEnum.FALSE.value());
+
+        Const.ResultEnum state = Const.ResultEnum.FAIL;
+
+        //必填,请参考"开发准备"获取如下数据,替换为实际值
+        //APP接入地址(在控制台"应用管理"页面获取)+接口访问URI
+        String url = "https://smsapi." + smsProps.getRegionId() + ".myhuaweicloud.com:443/sms/batchSendSms/v1";
+        String appKey = smsProps.getAppKey(); //APP_Key
+        String appSecret = smsProps.getAppSecret(); //APP_Secret
+        String sender = smsProps.getChannelId(); //国内短信签名通道号或国际/港澳台短信通道号
+        String templateId = smsProps.getTplId(); //模板ID
+
+        //条件必填,国内短信关注,当templateId指定的模板类型为通用模板时生效且必填,必须是已审核通过的,与模板类型一致的签名名称
+        //国际/港澳台短信不用关注该参数
+        String signature = smsProps.getSign(); //签名名称
+
+        //必填,全局号码格式(包含国家码),示例:+8615123456789,多个号码之间用英文逗号分隔
+        String receiver = request.getMailTo(); //短信接收人号码
+
+        //选填,短信状态报告接收地址,推荐使用域名,为空或者不填表示不接收状态报告
+        String statusCallBack = "";
+
+        /**
+         * 选填,使用无变量模板时请赋空值 String templateParas = "";
+         * 单变量模板示例:模板内容为"您的验证码是${1}"时,templateParas可填写为"[\"369751\"]"
+         * 双变量模板示例:模板内容为"您有${1}件快递请到${2}领取"时,templateParas可填写为"[\"3\",\"人民公园正门\"]"
+         * 模板中的每个变量都必须赋值，且取值不能为空
+         * 查看更多模板和变量规范:产品介绍>模板和变量规范
+         */
+        String templateParas = request.getContentParam(); //模板变量，此处以单变量验证码短信为例，请客户自行生成6位验证码，并定义为字符串类型，以杜绝首位0丢失的问题（例如：002569变成了2569）。
+
+        //请求Body,不携带签名名称时,signature请填null
+        String body = buildRequestBody(sender, receiver, templateId, templateParas, statusCallBack, signature);
+        if (null == body || body.isEmpty()) {
+            System.out.println("body is null.");
+            return false;
+        }
+
+        //请求Headers中的X-WSSE参数值
+        String wsseHeader = buildWsseHeader(appKey, appSecret);
+        if (null == wsseHeader || wsseHeader.isEmpty()) {
+            System.out.println("wsse header is null.");
+            return false;
+        }
+
+        Writer out = null;
+        BufferedReader in = null;
+        StringBuffer result = new StringBuffer();
+        HttpsURLConnection connection = null;
+        InputStream is = null;
+
+        HostnameVerifier hv = (hostname, session) -> true;
+        try {
+            trustAllHttpsCertificates();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        try {
+            URL realUrl = new URL(url);
+            connection = (HttpsURLConnection) realUrl.openConnection();
+
+            connection.setHostnameVerifier(hv);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(true);
+            //请求方法
+            connection.setRequestMethod("POST");
+            //请求Headers参数
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Authorization", AUTH_HEADER_VALUE);
+            connection.setRequestProperty("X-WSSE", wsseHeader);
+
+            connection.connect();
+            out = new OutputStreamWriter(connection.getOutputStream());
+            out.write(body); //发送请求Body参数
+            out.flush();
+            out.close();
+
+            int status = connection.getResponseCode();
+            if (200 == status) { //200
+                is = connection.getInputStream();
+            } else { //400/401
+                is = connection.getErrorStream();
+            }
+            in = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line = "";
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+            state = Const.ResultEnum.SUCCESS;
+            mailLog.setResult(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.close();
+                }
+                if (null != is) {
+                    is.close();
+                }
+                if (null != in) {
+                    in.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        mailLog.setState(state.value());
+        mailLogService.save(mailLog);
+        return mailLog.getState() == Const.ResultEnum.SUCCESS.value();
+    }
+
+    /**
+     * 构造请求Body体
+     */
+    static String buildRequestBody(String sender, String receiver, String templateId, String templateParas,
+                                   String statusCallBack, String signature) {
+        if (null == sender || null == receiver || null == templateId || sender.isEmpty() || receiver.isEmpty()
+                || templateId.isEmpty()) {
+            System.out.println("buildRequestBody(): sender, receiver or templateId is null.");
+            return null;
+        }
+        Map<String, String> map = new HashMap<>();
+
+        map.put("from", sender);
+        map.put("to", receiver);
+        map.put("templateId", templateId);
+        if (null != templateParas && !templateParas.isEmpty()) {
+            map.put("templateParas", templateParas);
+        }
+        if (null != statusCallBack && !statusCallBack.isEmpty()) {
+            map.put("statusCallback", statusCallBack);
+        }
+        if (null != signature && !signature.isEmpty()) {
+            map.put("signature", signature);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String temp = "";
+
+        for (String s : map.keySet()) {
+            try {
+                temp = URLEncoder.encode(map.get(s), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            sb.append(s).append("=").append(temp).append("&");
+        }
+
+        return sb.deleteCharAt(sb.length()-1).toString();
+    }
+
+    /**
+     * 构造X-WSSE参数值
+     */
+    static String buildWsseHeader(String appKey, String appSecret) {
+        if (null == appKey || null == appSecret || appKey.isEmpty() || appSecret.isEmpty()) {
+            System.out.println("buildWsseHeader(): appKey or appSecret is null.");
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        String time = sdf.format(new Date()); //Created
+        String nonce = UUID.randomUUID().toString().replace("-", ""); //Nonce
+
+        MessageDigest md;
+        byte[] passwordDigest = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update((nonce + time + appSecret).getBytes());
+            passwordDigest = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // 如果JDK版本是1.8,请加载原生Base64类,并使用如下代码
+        String passwordDigestBase64Str = Base64.getEncoder().encodeToString(passwordDigest); //PasswordDigest
+        // 如果JDK版本低于1.8,请加载三方库提供Base64类,并使用如下代码
+        // String passwordDigestBase64Str = Base64.encodeBase64String(passwordDigest); //PasswordDigest
+        // 若passwordDigestBase64Str中包含换行符,请执行如下代码进行修正
+        // passwordDigestBase64Str = passwordDigestBase64Str.replaceAll("[\\s*\t\n\r]", "");
+        return String.format(WSSE_HEADER_FORMAT, appKey, passwordDigestBase64Str, nonce, time);
+    }
+
+    private void trustAllHttpsCertificates() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        return;
+                    }
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        return;
+                    }
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                }
+        };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 
 }
