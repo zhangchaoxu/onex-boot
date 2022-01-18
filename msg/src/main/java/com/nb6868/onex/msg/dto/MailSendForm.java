49767be5edf03932ddfc6747cc058690e961e02c
Charles <zhangchaoxu@gmail.com>
2022-01-18 10:58:38 +0800

    remove send request to form

--- msg/src/main/java/com/nb6868/onex/msg/dto/MailSendForm.java
+++ msg/src/main/java/com/nb6868/onex/msg/dto/MailSendForm.java
@@ -1,6 +1,7 @@
 package com.nb6868.onex.msg.dto;
 
 import com.nb6868.onex.common.validator.group.AddGroup;
+import com.nb6868.onex.common.validator.group.DefaultGroup;
 import com.nb6868.onex.msg.MsgConst;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -8,27 +9,21 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.io.File;
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * 消息发送请求
- *
- * @author Charles zhangchaoxu@gmail.com
- */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @ApiModel(value = "消息发送请求")
-public class MailSendRequest implements Serializable {
+public class MailSendForm implements Serializable {
 
     @ApiModelProperty(value = "模板编码", required = true)
-    @NotBlank(message = "模板编码不能为空", groups = AddGroup.class)
+    @NotBlank(message = "模板编码不能为空", groups = DefaultGroup.class)
     private String tplCode = MsgConst.SMS_TPL_LOGIN;
 
     @ApiModelProperty(value = "收件人", required = true)
-    @NotBlank(message = "收件人不能为空", groups = AddGroup.class)
+    @NotBlank(message = "收件人不能为空", groups = DefaultGroup.class)
     private String mailTo;
 
     @ApiModelProperty(value = "抄送人")
