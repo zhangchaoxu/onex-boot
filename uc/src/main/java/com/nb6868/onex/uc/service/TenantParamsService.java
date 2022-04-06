d0574464a034a9e06eec1dd2e461d7fbd49d4523
Charles <zhangchaoxu@gmail.com>
2022-04-06 14:57:15 +0800

    change uc module

--- uc/src/main/java/com/nb6868/onex/uc/service/TenantParamsService.java
+++ uc/src/main/java/com/nb6868/onex/uc/service/TenantParamsService.java
@@ -1,5 +1,6 @@
 package com.nb6868.onex.uc.service;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import com.nb6868.onex.common.jpa.EntityService;
 import com.nb6868.onex.common.pojo.Const;
@@ -21,10 +22,20 @@ public class TenantParamsService extends EntityService<TenantParamsDao, TenantPa
      * @param code 参数编码
      */
     public JSONObject getContent(String tenantCode, String code) {
+        return getContent(tenantCode, code, null, null);
+    }
+
+    /**
+     * 根据参数编码，获取参数的value值
+     *
+     * @param code 参数编码
+     */
+    public JSONObject getContent(String tenantCode, String code, String contentJsonKey, String contentJsonValue) {
         return query()
                 .select("content")
-                .eq("tenant_code", tenantCode)
-                .eq("code", code)
+                .eq(StrUtil.isNotBlank(tenantCode), "tenant_code", tenantCode)
+                .eq(StrUtil.isNotBlank(code), "code", code)
+                .eq(StrUtil.isNotBlank(contentJsonKey) && StrUtil.isNotBlank(contentJsonValue), "content->'$." + contentJsonKey + "'", contentJsonValue)
                 .last(Const.LIMIT_ONE)
                 .oneOpt()
                 .map(TenantParamsEntity::getContent)
