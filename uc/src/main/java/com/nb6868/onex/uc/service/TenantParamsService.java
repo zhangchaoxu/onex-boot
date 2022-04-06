00f3e8c503c553a52926af3d0ea7cd6fae0fb3df
Charles <zhangchaoxu@gmail.com>
2022-04-06 11:00:06 +0800

    add tenant params

---
+++ uc/src/main/java/com/nb6868/onex/uc/service/TenantParamsService.java
@@ -0,0 +1,17 @@
+package com.nb6868.onex.uc.service;
+
+import com.nb6868.onex.common.jpa.EntityService;
+import com.nb6868.onex.uc.dao.TenantParamsDao;
+import com.nb6868.onex.uc.entity.TenantParamsEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * 租户参数
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+@Service
+public class TenantParamsService extends EntityService<TenantParamsDao, TenantParamsEntity> {
+
+
+}
