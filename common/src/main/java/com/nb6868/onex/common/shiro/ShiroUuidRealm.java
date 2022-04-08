81f54f500b1a925616b1d148d261d68ed018f8ea
Charles <zhangchaoxu@gmail.com>
2022-04-08 10:24:42 +0800

    - add: user add area_code - enhance: simplify shiro code

--- common/src/main/java/com/nb6868/onex/common/shiro/ShiroUuidRealm.java
+++ common/src/main/java/com/nb6868/onex/common/shiro/ShiroUuidRealm.java
@@ -4,17 +4,19 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.jwt.JWT;
 import com.nb6868.onex.common.auth.AuthProps;
 import com.nb6868.onex.common.exception.ErrorCode;
-import com.nb6868.onex.common.util.JwtUtils;
 import com.nb6868.onex.common.validator.AssertUtils;
-import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -23,12 +25,13 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Shiro认证
+ * ShiroUuidRealm
  *
  * @author Charles zhangchaoxu@gmail.com
  */
 @Component
-public class ShiroRealm extends AuthorizingRealm {
+@ConditionalOnProperty(name = "onex.shiro.type", havingValue = "uuid")
+public class ShiroUuidRealm extends AuthorizingRealm {
 
     @Autowired
     private AuthProps authProps;
@@ -51,28 +54,16 @@ public class ShiroRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         // AuthenticationToken包含身份信息和认证信息
         String token = authenticationToken.getCredentials().toString();
-        AuthProps.Config loginConfig = null;
-        if (authenticationToken.getPrincipal() instanceof AuthProps.Config) {
-            loginConfig = (AuthProps.Config) authenticationToken.getPrincipal();
-        }
-        AssertUtils.isNull(loginConfig, ErrorCode.UNAUTHORIZED);
-        // 尝试解析为jwt
-        JWT jwt = JwtUtils.parseToken(token);
-        AssertUtils.isNull(jwt, ErrorCode.UNAUTHORIZED);
-
-        // 获取用户id
-        Long userId;
-        if ("db".equalsIgnoreCase(loginConfig.getTokenStoreType())) {
-            // token存在数据库中
-            Map<String, Object> tokenEntity = shiroDao.getUserTokenByToken(token);
-            AssertUtils.isNull(tokenEntity, ErrorCode.UNAUTHORIZED);
-            userId = MapUtil.getLong(tokenEntity, "user_id");
-        } else {
-            // token没有持久化，直接用jwt验证
-            AssertUtils.isFalse(jwt.setKey(loginConfig.getTokenKey().getBytes()).validate(0), ErrorCode.UNAUTHORIZED);
-            userId = jwt.getPayload().getClaimsJson().getLong("id");
-        }
+        // token存在数据库中
+        Map<String, Object> tokenEntity = shiroDao.getUserTokenByToken(token);
+        AssertUtils.isNull(tokenEntity, ErrorCode.UNAUTHORIZED);
+        Long userId = MapUtil.getLong(tokenEntity, "user_id");
         AssertUtils.isNull(userId, ErrorCode.UNAUTHORIZED);
+        String loginType = MapUtil.getStr(tokenEntity, "type");
+        // 获取jwt中的登录配置
+        AuthProps.Config loginConfig = authProps.getConfigs().get(loginType);
+        AssertUtils.isNull(loginConfig, ErrorCode.UNAUTHORIZED);
+
         // 验证用户是否还存在
         Map<String, Object> userEntity = shiroDao.getUserById(userId);
         // 账号不存在
@@ -82,7 +73,7 @@ public class ShiroRealm extends AuthorizingRealm {
         // 转换成UserDetail对象
         ShiroUser shiroUser = BeanUtil.mapToBean(userEntity, ShiroUser.class, true, CopyOptions.create().setIgnoreCase(true));
         shiroUser.setLoginType(loginConfig.getType());
-        if ("db".equalsIgnoreCase(loginConfig.getTokenStoreType()) && loginConfig.isTokenRenewal()) {
+        if (loginConfig.isTokenRenewal()) {
             // 更新token
             shiroDao.updateTokenExpireTime(token, loginConfig.getTokenExpire());
         }
