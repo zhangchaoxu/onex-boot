32984cb9c7e3d5cbaf16df37393de42f9482edb7
Charles <zhangchaoxu@gmail.com>
2022-04-11 19:27:00 +0800

    fix log empty tenant code issue

--- sys/src/main/java/com/nb6868/onex/sys/SysConst.java
+++ sys/src/main/java/com/nb6868/onex/sys/SysConst.java
@@ -7,5 +7,37 @@ package com.nb6868.onex.sys;
  */
 public interface SysConst {
 
+    /**
+     * 日志类型
+     */
+    enum LogTypeEnum {
+
+        /**
+         * 详见name
+         */
+        LOGIN("login", "登录"),
+        OPERATION("operation", "操作"),
+        ERROR("error", "错误");
+
+        private String value;
+        private String name;
+
+        LogTypeEnum(String value) {
+            this.value = value;
+        }
+
+        LogTypeEnum(String value, String name) {
+            this.value = value;
+            this.name = name;
+        }
+
+        public String value() {
+            return this.value;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
 
 }
