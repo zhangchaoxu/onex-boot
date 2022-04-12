3e4da0efc77fc868adb8945f0c2189434d6fa889
Charles <zhangchaoxu@gmail.com>
2022-04-12 15:00:09 +0800

    add tenant query

--- uc/src/main/java/com/nb6868/onex/uc/controller/TenantController.java
+++ uc/src/main/java/com/nb6868/onex/uc/controller/TenantController.java
@@ -1,17 +1,23 @@
 package com.nb6868.onex.uc.controller;
 
 import cn.hutool.json.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nb6868.onex.common.annotation.LogOperation;
 import com.nb6868.onex.common.exception.ErrorCode;
+import com.nb6868.onex.common.jpa.QueryWrapperHelper;
+import com.nb6868.onex.common.pojo.IdForm;
 import com.nb6868.onex.common.pojo.PageData;
 import com.nb6868.onex.common.pojo.Result;
 import com.nb6868.onex.common.shiro.ShiroUtils;
 import com.nb6868.onex.common.validator.AssertUtils;
 import com.nb6868.onex.common.validator.group.AddGroup;
 import com.nb6868.onex.common.validator.group.DefaultGroup;
+import com.nb6868.onex.common.validator.group.PageGroup;
 import com.nb6868.onex.common.validator.group.UpdateGroup;
 import com.nb6868.onex.uc.dto.TenantDTO;
 import com.nb6868.onex.uc.dto.TenantParamsInfoByCodeForm;
+import com.nb6868.onex.uc.dto.TenantQueryForm;
+import com.nb6868.onex.uc.entity.TenantEntity;
 import com.nb6868.onex.uc.service.ParamsService;
 import com.nb6868.onex.uc.service.TenantService;
 import io.swagger.annotations.Api;
@@ -44,8 +50,9 @@ public class TenantController {
     @GetMapping("list")
     @ApiOperation("列表")
     @RequiresPermissions("uc:tenant:info")
-    public Result<?> list(@ApiIgnore @RequestParam Map<String, Object> params) {
-        List<TenantDTO> list = tenantService.listDto(params);
+    public Result<?> list(@Validated @RequestBody TenantQueryForm form) {
+        QueryWrapper<TenantEntity> queryWrapper = QueryWrapperHelper.getPredicate(form);
+        List<TenantDTO> list = tenantService.listDto(queryWrapper);
 
         return new Result<>().success(list);
     }
@@ -53,8 +60,9 @@ public class TenantController {
     @GetMapping("page")
     @ApiOperation("分页")
     @RequiresPermissions("uc:tenant:info")
-    public Result<?> page(@ApiIgnore @RequestParam Map<String, Object> params) {
-        PageData<TenantDTO> page = tenantService.pageDto(params);
+    public Result<?> page(@Validated({PageGroup.class}) @RequestBody TenantQueryForm form) {
+        QueryWrapper<TenantEntity> queryWrapper = QueryWrapperHelper.getPredicate(form);
+        PageData<TenantDTO> page = tenantService.pageDto(form.getPage(), queryWrapper);
 
         return new Result<>().success(page);
     }
@@ -62,8 +70,8 @@ public class TenantController {
     @GetMapping("info")
     @ApiOperation("信息")
     @RequiresPermissions("uc:tenant:info")
-    public Result<?> info(@RequestParam @NotNull(message = "{id.require}") Long id) {
-        TenantDTO data = tenantService.getDtoById(id);
+    public Result<?> info(@Validated @RequestBody IdForm form) {
+        TenantDTO data = tenantService.getDtoById(form.getId());
         AssertUtils.isNull(data, ErrorCode.DB_RECORD_NOT_EXISTED);
 
         return new Result<TenantDTO>().success(data);
@@ -93,8 +101,8 @@ public class TenantController {
     @ApiOperation("删除")
     @LogOperation("删除")
     @RequiresPermissions("uc:tenant:delete")
-    public Result<?> delete(@NotNull(message = "{id.require}") @RequestParam Long id) {
-        tenantService.logicDeleteById(id);
+    public Result<?> delete(@Validated @RequestBody IdForm form) {
+        tenantService.logicDeleteById(form.getId());
 
         return new Result<>();
     }
