e590ba855af3838fe7e27ab5969a554aef0576b7
Charles <zhangchaoxu@gmail.com>
2022-05-20 18:44:37 +0800

    add exception msg in property

--- sched/src/main/java/com/nb6868/onex/sys/dto/SchedDTO.java
+++ sched/src/main/java/com/nb6868/onex/sys/dto/SchedDTO.java
@@ -2,12 +2,12 @@ package com.nb6868.onex.sys.dto;
 
 import cn.hutool.json.JSONObject;
 import com.nb6868.onex.common.pojo.BaseDTO;
+import com.nb6868.onex.common.validator.EnumValue;
 import com.nb6868.onex.common.validator.group.DefaultGroup;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
 
@@ -26,7 +26,7 @@ public class SchedDTO extends BaseDTO {
 	private String name;
 
 	@ApiModelProperty(value = "状态")
-	@Range(min = 0, max = 1, message = "状态值错误", groups = DefaultGroup.class)
+	@EnumValue(intValues = {0, 1}, message = "状态值错误", groups = DefaultGroup.class)
 	private Integer state;
 
 	@ApiModelProperty(value = "cron表达式")
@@ -38,7 +38,7 @@ public class SchedDTO extends BaseDTO {
 
 	@ApiModelProperty(value = "日志类型")
 	@NotBlank(message = "日志类型不能为空", groups = DefaultGroup.class)
-	private String logType;
+	private String logType = "db";
 
 	@ApiModelProperty(value = "参数")
 	private JSONObject params;
