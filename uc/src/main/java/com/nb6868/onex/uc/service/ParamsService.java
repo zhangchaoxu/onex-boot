a3ab207477dd0ef80ac221ef0edc8356d6e47864
Charles <zhangchaoxu@gmail.com>
2022-06-01 20:02:07 +0800

    add BaseParamsService

--- uc/src/main/java/com/nb6868/onex/uc/service/ParamsService.java
+++ uc/src/main/java/com/nb6868/onex/uc/service/ParamsService.java
@@ -1,6 +1,7 @@
 package com.nb6868.onex.uc.service;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import com.nb6868.onex.common.jpa.DtoService;
 import com.nb6868.onex.common.params.BaseParamsService;
 import com.nb6868.onex.common.pojo.Const;
@@ -19,32 +20,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ParamsService extends DtoService<ParamsDao, ParamsEntity, ParamsDTO> implements BaseParamsService {
 
-    /**
-     * 获得用户参数
-     */
+
+    @Override
     public String getSystemContent(String code) {
         return getContent(UcConst.ParamsTypeEnum.SYSTEM.value(), null, null, code, null, null);
     }
 
-    /**
-     * 获得用户参数
-     */
+    @Override
     public String getUserContent(Long userId, String code) {
         return getContent(UcConst.ParamsTypeEnum.USER.value(), null, userId, code, null, null);
     }
 
-    /**
-     * 获得租户参数
-     */
+    @Override
     public String getTenantContent(String tenantCode, String code) {
         return getContent(UcConst.ParamsTypeEnum.TENANT.value(), tenantCode, null, code, null, null);
     }
 
-    /**
-     * 根据参数编码，获取参数的value值
-     *
-     * @param code 参数编码
-     */
+    @Override
     public String getContent(Integer type, String tenantCode, Long userId, String code) {
         return getContent(type, tenantCode, userId, code, null, null);
     }
@@ -54,6 +46,7 @@ public class ParamsService extends DtoService<ParamsDao, ParamsEntity, ParamsDTO
      *
      * @param code 参数编码
      */
+    @Override
     public String getContent(Integer type, String tenantCode, Long userId, String code, String contentJsonKey, String contentJsonValue) {
         return query().select("content")
                 .eq(StrUtil.isNotBlank(tenantCode), "tenant_code", tenantCode)
@@ -67,36 +60,42 @@ public class ParamsService extends DtoService<ParamsDao, ParamsEntity, ParamsDTO
                 .orElse(null);
     }
 
-    /**
-     * 获得用户参数
-     */
+    @Override
+    public JSONObject getSystemContentJson(String code) {
+        return getSystemContentObject(code, JSONObject.class, null);
+    }
+
+    @Override
+    public JSONObject getUserContentJson(Long userId, String code) {
+        return getUserContentObject(userId, code, JSONObject.class, null);
+    }
+
+    @Override
+    public JSONObject getTenantContentJson(String tenantCode, String code) {
+        return getTenantContentObject(tenantCode, code, JSONObject.class, null);
+    }
+
+    @Override
     public <T> T getSystemContentObject(String code, Class<T> clazz, T defObj) {
         return getContentObject(UcConst.ParamsTypeEnum.SYSTEM.value(), null, null, code, null, null, clazz, defObj);
     }
 
-    /**
-     * 获得用户参数
-     */
+    @Override
     public <T> T getUserContentObject(Long userId, String code, Class<T> clazz, T defObj) {
         return getContentObject(UcConst.ParamsTypeEnum.USER.value(), null, userId, code, null, null, clazz, defObj);
     }
 
-    /**
-     * 获得租户参数
-     */
+    @Override
     public <T> T getTenantContentObject(String tenantCode, String code, Class<T> clazz, T defObj) {
         return getContentObject(UcConst.ParamsTypeEnum.TENANT.value(), tenantCode, null, code, null, null, clazz, defObj);
     }
 
-    /**
-     * 根据参数编码，获取参数的value值
-     *
-     * @param code 参数编码
-     */
+    @Override
     public <T> T getContentObject(Integer type, String tenantCode, Long userId, String code, Class<T> clazz, T defObj) {
         return getContentObject(type, tenantCode, userId, code, null, null, clazz, defObj);
     }
 
+    @Override
     public <T> T getContentObject(Integer type, String tenantCode, Long userId, String code, String contentJsonKey, String contentJsonValue, Class<T> clazz, T defObj) {
         String content = getContent(type, tenantCode, userId, code, contentJsonKey, contentJsonValue);
         return JacksonUtils.jsonToPojo(content, clazz, defObj);
