8e7b48b0a21e6db696bcb574376db08b1fd2f502
Charles <zhangchaoxu@gmail.com>
2022-06-02 19:05:29 +0800

    fix sys method issue

--- sys/src/main/java/com/nb6868/onex/sys/controller/DictController.java
+++ sys/src/main/java/com/nb6868/onex/sys/controller/DictController.java
@@ -1,62 +1,61 @@
 package com.nb6868.onex.sys.controller;
 
 import com.nb6868.onex.common.annotation.LogOperation;
-import com.nb6868.onex.sys.dto.DictDTO;
-import com.nb6868.onex.sys.service.DictService;
 import com.nb6868.onex.common.exception.ErrorCode;
+import com.nb6868.onex.common.jpa.QueryWrapperHelper;
+import com.nb6868.onex.common.pojo.IdForm;
+import com.nb6868.onex.common.pojo.IdsForm;
 import com.nb6868.onex.common.pojo.PageData;
 import com.nb6868.onex.common.pojo.Result;
 import com.nb6868.onex.common.validator.AssertUtils;
 import com.nb6868.onex.common.validator.group.AddGroup;
 import com.nb6868.onex.common.validator.group.DefaultGroup;
+import com.nb6868.onex.common.validator.group.PageGroup;
 import com.nb6868.onex.common.validator.group.UpdateGroup;
+import com.nb6868.onex.sys.dto.DictDTO;
+import com.nb6868.onex.sys.dto.DictQueryForm;
+import com.nb6868.onex.sys.service.DictService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Map;
 
-/**
- * 数据字典
- */
 @RestController
 @RequestMapping("/sys/dict")
 @Validated
 @Api(tags="数据字典", position = 50)
 public class DictController {
+
     @Autowired
     private DictService dictService;
 
-    @GetMapping("page")
+    @PostMapping("page")
     @ApiOperation("字典分类")
-    @RequiresPermissions("sys:dict:page")
-    public Result<?> page(@ApiIgnore @RequestParam Map<String, Object> params){
-        PageData<DictDTO> page = dictService.pageDto(params);
+    @RequiresPermissions("sys:dict:query")
+    public Result<?> page(@Validated(PageGroup.class) @RequestBody DictQueryForm form) {
+        PageData<?> page = dictService.pageDto(form.getPage(), QueryWrapperHelper.getPredicate(form, "page"));
 
         return new Result<>().success(page);
     }
 
-    @GetMapping("list")
+    @PostMapping("list")
     @ApiOperation("字典分类数据")
-    @RequiresPermissions("sys:dict:list")
-    public Result<?> list(@ApiIgnore @RequestParam Map<String, Object> params){
-        List<DictDTO> list = dictService.listDto(params);
+    @RequiresPermissions("sys:dict:query")
+    public Result<?> list(@Validated @RequestBody DictQueryForm form) {
+        List<?> list = dictService.listDto(QueryWrapperHelper.getPredicate(form, "list"));
 
-        return new Result<List<DictDTO>>().success(list);
+        return new Result<>().success(list);
     }
 
-    @GetMapping("info")
+    @PostMapping("info")
     @ApiOperation("信息")
-    @RequiresPermissions("sys:dict:info")
-    public Result<DictDTO> info(@NotNull(message = "{id.require}") @RequestParam Long id){
-        DictDTO data = dictService.getDtoById(id);
+    @RequiresPermissions("sys:dict:query")
+    public Result<DictDTO> info(@Validated @RequestBody IdForm form) {
+        DictDTO data = dictService.oneDto(QueryWrapperHelper.getPredicate(form));
         AssertUtils.isNull(data, ErrorCode.DB_RECORD_NOT_EXISTED);
 
         return new Result<DictDTO>().success(data);
@@ -65,39 +64,39 @@ public class DictController {
     @PostMapping("save")
     @ApiOperation("保存")
     @LogOperation("保存")
-    @RequiresPermissions("sys:dict:save")
+    @RequiresPermissions("sys:dict:edit")
     public Result<?> save(@Validated(value = {DefaultGroup.class, AddGroup.class}) @RequestBody DictDTO dto){
         dictService.saveDto(dto);
 
         return new Result<>();
     }
 
-    @PutMapping("update")
+    @PostMapping("update")
     @ApiOperation("修改")
     @LogOperation("修改")
-    @RequiresPermissions("sys:dict:update")
+    @RequiresPermissions("sys:dict:edit")
     public Result<?> update(@Validated(value = {DefaultGroup.class, UpdateGroup.class}) @RequestBody DictDTO dto){
         dictService.updateDto(dto);
 
         return new Result<>();
     }
 
-    @DeleteMapping("delete")
+    @PostMapping("delete")
     @ApiOperation("删除")
     @LogOperation("删除")
     @RequiresPermissions("sys:dict:delete")
-    public Result<?> delete(@NotNull(message = "{id.require}") @RequestParam Long id){
-        dictService.logicDeleteById(id);
+    public Result<?> delete(@Validated @RequestBody IdForm form) {
+        dictService.logicDeleteById(form.getId());
 
         return new Result<>();
     }
 
-    @DeleteMapping("deleteBatch")
+    @PostMapping("deleteBatch")
     @ApiOperation("批量删除")
     @LogOperation("批量删除")
-    @RequiresPermissions("sys:dict:deleteBatch")
-    public Result<?> deleteBatch(@NotEmpty(message = "{ids.require}") @RequestBody List<Long> ids){
-        dictService.logicDeleteByIds(ids);
+    @RequiresPermissions("sys:dict:delete")
+    public Result<?> deleteBatch(@Validated @RequestBody IdsForm form) {
+        dictService.logicDeleteByIds(form.getIds());
 
         return new Result<>();
     }
--- sys/src/main/java/com/nb6868/onex/sys/controller/RegionController.java
+++ sys/src/main/java/com/nb6868/onex/sys/controller/RegionController.java
@@ -5,31 +5,30 @@ import cn.hutool.core.lang.tree.Tree;
 import cn.hutool.core.lang.tree.TreeNode;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nb6868.onex.common.annotation.LogOperation;
+import com.nb6868.onex.common.exception.ErrorCode;
 import com.nb6868.onex.common.jpa.QueryWrapperHelper;
+import com.nb6868.onex.common.pojo.IdForm;
+import com.nb6868.onex.common.pojo.PageData;
+import com.nb6868.onex.common.pojo.Result;
 import com.nb6868.onex.common.util.TreeNodeUtils;
+import com.nb6868.onex.common.validator.AssertUtils;
+import com.nb6868.onex.common.validator.group.AddGroup;
+import com.nb6868.onex.common.validator.group.DefaultGroup;
+import com.nb6868.onex.common.validator.group.PageGroup;
+import com.nb6868.onex.common.validator.group.UpdateGroup;
 import com.nb6868.onex.sys.dto.RegionDTO;
 import com.nb6868.onex.sys.dto.RegionQueryForm;
 import com.nb6868.onex.sys.entity.RegionEntity;
 import com.nb6868.onex.sys.service.RegionService;
-import com.nb6868.onex.common.exception.ErrorCode;
-import com.nb6868.onex.common.pojo.PageData;
-import com.nb6868.onex.common.pojo.Result;
-import com.nb6868.onex.common.validator.AssertUtils;
-import com.nb6868.onex.common.validator.group.AddGroup;
-import com.nb6868.onex.common.validator.group.DefaultGroup;
-import com.nb6868.onex.common.validator.group.UpdateGroup;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 行政区域
@@ -56,29 +55,29 @@ public class RegionController {
         return new Result<>().success(treeList);
     }
 
-    @GetMapping("list")
+    @PostMapping("list")
     @ApiOperation("列表")
     @RequiresPermissions("sys:region:query")
-    public Result<?> list(@ApiIgnore @RequestParam Map<String, Object> params) {
-        List<RegionDTO> list = regionService.listDto(params);
+    public Result<?> list(@Validated @RequestBody RegionQueryForm form) {
+        List<?> list = regionService.listDto(QueryWrapperHelper.getPredicate(form, "list"));
 
         return new Result<>().success(list);
     }
 
-    @GetMapping("page")
+    @PostMapping("page")
     @ApiOperation("分页")
     @RequiresPermissions("sys:region:query")
-    public Result<?> page(@ApiIgnore @RequestParam Map<String, Object> params) {
-        PageData<RegionDTO> page = regionService.pageDto(params);
+    public Result<?> page(@Validated(PageGroup.class) @RequestBody RegionQueryForm form) {
+        PageData<?> page = regionService.pageDto(form.getPage(), QueryWrapperHelper.getPredicate(form, "page"));
 
         return new Result<>().success(page);
     }
 
-    @GetMapping("info")
+    @PostMapping("info")
     @ApiOperation("信息")
     @RequiresPermissions("sys:region:query")
-    public Result<?> info(@NotNull(message = "{id.require}") @RequestParam Long id) {
-        RegionDTO data = regionService.getDtoById(id);
+    public Result<?>info(@Validated @RequestBody IdForm form) {
+        RegionDTO data = regionService.oneDto(QueryWrapperHelper.getPredicate(form));
         AssertUtils.isNull(data, ErrorCode.DB_RECORD_NOT_EXISTED);
 
         return new Result<>().success(data);
@@ -94,7 +93,7 @@ public class RegionController {
         return new Result<>().success(dto);
     }
 
-    @PutMapping("update")
+    @PostMapping("update")
     @ApiOperation("修改")
     @LogOperation("修改")
     @RequiresPermissions("sys:region:edit")
@@ -104,12 +103,12 @@ public class RegionController {
         return new Result<>().success(dto);
     }
 
-    @DeleteMapping("delete")
+    @PostMapping("delete")
     @ApiOperation("删除")
     @LogOperation("删除")
     @RequiresPermissions("sys:region:delete")
-    public Result<?> delete(@NotNull(message = "{id.require}") @RequestParam Long id) {
-        regionService.deleteById(id);
+    public Result<?> delete(@Validated @RequestBody IdForm form) {
+        regionService.logicDeleteById(form.getId());
 
         return new Result<>();
     }
