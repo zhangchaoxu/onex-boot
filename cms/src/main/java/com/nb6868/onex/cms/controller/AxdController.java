3cf11cb8b99de15d8f2c2f39914705348bd23ade
Charles <zhangchaoxu@gmail.com>
2022-06-02 19:19:18 +0800

    fix cms issue

--- cms/src/main/java/com/nb6868/onex/cms/controller/AxdController.java
+++ cms/src/main/java/com/nb6868/onex/cms/controller/AxdController.java
@@ -1,14 +1,20 @@
 package com.nb6868.onex.cms.controller;
 
 import com.nb6868.onex.cms.dto.AxdDTO;
+import com.nb6868.onex.cms.dto.AxdQueryForm;
 import com.nb6868.onex.cms.service.AxdService;
 import com.nb6868.onex.common.annotation.LogOperation;
+import com.nb6868.onex.common.annotation.QueryDataScope;
 import com.nb6868.onex.common.exception.ErrorCode;
+import com.nb6868.onex.common.jpa.QueryWrapperHelper;
+import com.nb6868.onex.common.pojo.IdTenantForm;
+import com.nb6868.onex.common.pojo.IdsTenantForm;
 import com.nb6868.onex.common.pojo.PageData;
 import com.nb6868.onex.common.pojo.Result;
 import com.nb6868.onex.common.validator.AssertUtils;
 import com.nb6868.onex.common.validator.group.AddGroup;
 import com.nb6868.onex.common.validator.group.DefaultGroup;
+import com.nb6868.onex.common.validator.group.PageGroup;
 import com.nb6868.onex.common.validator.group.UpdateGroup;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,12 +22,10 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 广告位
@@ -37,29 +41,32 @@ public class AxdController {
     @Autowired
     private AxdService axdService;
 
-    @GetMapping("list")
+    @PostMapping("list")
     @ApiOperation("列表")
-    @RequiresPermissions("cms:axd:list")
-    public Result<?> list(@ApiIgnore @RequestParam Map<String, Object> params) {
-        List<AxdDTO> list = axdService.listDto(params);
+    @RequiresPermissions("cms:axd:query")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
+    public Result<?> list(@Validated @RequestBody AxdQueryForm form) {
+        List<?> list = axdService.listDto(QueryWrapperHelper.getPredicate(form, "list"));
 
         return new Result<>().success(list);
     }
 
-    @GetMapping("page")
+    @PostMapping("page")
     @ApiOperation("分页")
-    @RequiresPermissions("cms:axd:page")
-    public Result<?> page(@ApiIgnore @RequestParam Map<String, Object> params) {
-        PageData<AxdDTO> page = axdService.pageDto(params);
+    @RequiresPermissions("cms:axd:query")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
+    public Result<?> page(@Validated({PageGroup.class}) @RequestBody AxdQueryForm form) {
+        PageData<?> page = axdService.pageDto(form.getPage(), QueryWrapperHelper.getPredicate(form, "page"));
 
         return new Result<>().success(page);
     }
 
-    @GetMapping("info")
+    @PostMapping("info")
     @ApiOperation("信息")
-    @RequiresPermissions("cms:axd:info")
-    public Result<?> info(@NotNull(message = "{id.require}") @RequestParam Long id) {
-        AxdDTO data = axdService.getDtoById(id);
+    @RequiresPermissions("cms:axd:query")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
+    public Result<?> info(@Validated @RequestBody IdTenantForm form) {
+        AxdDTO data = axdService.oneDto(QueryWrapperHelper.getPredicate(form));
         AssertUtils.isNull(data, ErrorCode.DB_RECORD_NOT_EXISTED);
 
         return new Result<>().success(data);
@@ -68,7 +75,8 @@ public class AxdController {
     @PostMapping("save")
     @ApiOperation("保存")
     @LogOperation("保存")
-    @RequiresPermissions("cms:axd:save")
+    @RequiresPermissions("cms:axd:edit")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
     public Result<?> save(@Validated(value = {DefaultGroup.class, AddGroup.class}) @RequestBody AxdDTO dto) {
         axdService.saveDto(dto);
 
@@ -78,7 +86,8 @@ public class AxdController {
     @PostMapping("update")
     @ApiOperation("修改")
     @LogOperation("修改")
-    @RequiresPermissions("cms:axd:update")
+    @RequiresPermissions("cms:axd:edit")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
     public Result<?> update(@Validated(value = {DefaultGroup.class, UpdateGroup.class}) @RequestBody AxdDTO dto) {
         axdService.updateDto(dto);
 
@@ -89,8 +98,9 @@ public class AxdController {
     @ApiOperation("删除")
     @LogOperation("删除")
     @RequiresPermissions("cms:axd:delete")
-    public Result<?> delete(@NotNull(message = "{id.require}") @RequestParam Long id) {
-        axdService.logicDeleteById(id);
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
+    public Result<?> delete(@Validated @RequestBody IdTenantForm form) {
+        axdService.logicDeleteByWrapper(QueryWrapperHelper.getPredicate(form));
 
         return new Result<>();
     }
@@ -98,9 +108,10 @@ public class AxdController {
     @PostMapping("deleteBatch")
     @ApiOperation("批量删除")
     @LogOperation("批量删除")
-    @RequiresPermissions("cms:axd:deleteBatch")
-    public Result<?> deleteBatch(@NotEmpty(message = "{ids.require}") @RequestBody List<Long> ids) {
-        axdService.logicDeleteByIds(ids);
+    @RequiresPermissions("cms:axd:delete")
+    @QueryDataScope(tenantFilter = true, tenantValidate = false)
+    public Result<?> deleteBatch(@Validated @RequestBody IdsTenantForm form) {
+        axdService.logicDeleteByWrapper(QueryWrapperHelper.getPredicate(form));
 
         return new Result<>();
     }
---
+++ cms/src/main/java/com/nb6868/onex/cms/dto/ArticleCategoryQueryForm.java
@@ -0,0 +1,19 @@
+package com.nb6868.onex.cms.dto;
+
+import com.nb6868.onex.common.jpa.Query;
+import com.nb6868.onex.common.pojo.BasePageForm;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "文章分类查询")
+public class ArticleCategoryQueryForm extends BasePageForm {
+
+    @Query(blurryType = Query.BlurryType.OR, type = Query.Type.LIKE, column = "name,code,descr,title")
+    @ApiModelProperty("搜索关键词")
+    private String search;
+
+}
