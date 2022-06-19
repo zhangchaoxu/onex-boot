8341da9a26d7153b84c2648ed406b885025ad560
Charles <zhangchaoxu@gmail.com>
2022-06-19 15:18:57 +0800

    add sys relation

---
+++ sys/src/main/java/com/nb6868/onex/sys/service/RelationService.java
@@ -0,0 +1,15 @@
+package com.nb6868.onex.sys.service;
+
+import com.nb6868.onex.common.jpa.EntityService;
+import com.nb6868.onex.sys.dao.RelationDao;
+import com.nb6868.onex.sys.entity.RelationEntity;
+import org.springframework.stereotype.Service;
+/**
+ * 系统-中间关系表
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
+@Service
+public class RelationService extends EntityService<RelationDao, RelationEntity> {
+
+}
