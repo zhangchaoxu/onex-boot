6ea915063c5d941b61dcd1296b0a86287138f560
Charles <zhangchaoxu@gmail.com>
2023-12-07 10:47:53 +0800

    edit springdoc

--- common/src/main/java/com/nb6868/onex/common/config/SwaggerConfig.java
+++ common/src/main/java/com/nb6868/onex/common/config/SwaggerConfig.java
@@ -3,7 +3,9 @@ package com.nb6868.onex.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -18,36 +20,6 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name = "onex.swagger.enable", havingValue = "true")
 public class SwaggerConfig {
 
-    /**
-     * 根据@Tag 上的排序，写入x-order
-     *
-     * @return the global open api customizer
-     */
-    /*@Bean
-    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
-        return openApi -> {
-            if (openApi.getTags() != null) {
-                openApi.getTags().forEach(tag -> {
-                    // 可以尝试将order写入description,反正也是没用的
-                    // 然后docs配置中将tags-sorter改为order
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("x-order", RandomUtil.randomInt(0, 100));
-                    tag.setExtensions(map);
-                });
-            }
-        };
-    }*/
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title(title)
-                        .version(version)
-                        .description(description))
-                .components(new Components().addSecuritySchemes(authTokenKey, new SecurityScheme()
-                        .name(authTokenKey).type(SecurityScheme.Type.HTTP)));
-    }
-
     @Value("${knife4j.title:OneX-API}")
     private String title;
     @Value("${knife4j.description:API}")
@@ -56,5 +28,50 @@ public class SwaggerConfig {
     private String version;
     @Value("${onex.auth.token-header-key:auth-token}")
     private String authTokenKey;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // 设置openapi基本信息
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description))
+                // 添加全局auth-token的头
+                .components(new Components().addSecuritySchemes(authTokenKey,
+                        new SecurityScheme()
+                                .name(authTokenKey)
+                                .in(SecurityScheme.In.HEADER)
+                                .type(SecurityScheme.Type.HTTP)));
+    }
+
+    /**
+     * 自定义openApi
+     */
+    @Bean
+    public GlobalOpenApiCustomizer customGlobalOpenApiCustomizer() {
+        return openApi -> {
+            // 对tag做处理,影响controller
+            if (openApi.getTags() != null) {
+                openApi.getTags().forEach(tag -> {
+                    // 对tags做排序,需要设置tags-sorter: order
+                    // tag.setExtensions(Map.of("x-order", tag.getDescription().charAt(0)));
+                });
+            }
+            // 对operations做处理,影响controller中的方法
+            if (openApi.getPaths() != null) {
+                openApi.getPaths().forEach((s, pathItem) -> pathItem.readOperations().forEach(operation -> {
+                    // 解决文档中请求头部不显示的问题
+                    // 可以在controller中加@SecurityRequirement(name ="auth-token")
+                    // 也可以在method中@Operation(summary = "分页", security = {@SecurityRequirement(name = "auth-token")})
+                    // 尝试在这里批量在Operation中添加SecurityRequirement
+                    operation.addSecurityItem(new SecurityRequirement().addList(authTokenKey));
+                    // 解决排序问题，需要设置operations-sorter: order
+                    // operation.setExtensions(Map.of("x-order", operation.getDescription().charAt(0)));
+                }));
+            }
+        };
+
+    }
 
 }
