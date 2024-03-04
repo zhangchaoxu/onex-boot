5fb030369f0eb1f166f667beedf9e49f50b871eb
Charles <zhangchaoxu@gmail.com>
2024-03-04 14:24:34 +0800

    add getPresignedUrl in aws-s3 service

--- common/src/main/java/com/nb6868/onex/common/oss/AwsS3Service.java
+++ common/src/main/java/com/nb6868/onex/common/oss/AwsS3Service.java
@@ -11,11 +11,15 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.time.Duration;
 
 /**
  * AWS S3 v2
@@ -26,7 +30,7 @@ import java.net.URI;
 @Slf4j
 public class AwsS3Service extends AbstractOssService {
 
-    S3Client s3Client;
+    private final S3Client s3Client;
 
     public AwsS3Service(OssPropsConfig config) {
         this.config = config;
@@ -98,9 +102,30 @@ public class AwsS3Service extends AbstractOssService {
         return config.getDomain() + objectKey;
     }
 
+    /**
+     * objectKey
+     * expire 有效时间(秒)
+     * see https://docs.aws.amazon.com/zh_cn/sdk-for-java/latest/developer-guide/examples-s3-presign.html
+     */
     @Override
-    public String getPresignedUrl(String objectName, Long expire) {
-        return null;
+    public String getPresignedUrl(String objectKey, Long expire) {
+        try {
+            S3Presigner presigner = S3Presigner.create();
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(config.getBucketName())
+                    .key(objectKey)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofSeconds(expire))
+                    .getObjectRequest(objectRequest)
+                    .build();
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+            // 生成以GET方法访问的签名URL，访客可以直接通过浏览器访问相关内容。
+            return presignedRequest.url().toExternalForm();
+        } catch (com.aliyun.oss.ClientException e) {
+            throw new OnexException(ErrorCode.OSS_UPLOAD_FILE_ERROR, e);
+        }
     }
 
     @Override
