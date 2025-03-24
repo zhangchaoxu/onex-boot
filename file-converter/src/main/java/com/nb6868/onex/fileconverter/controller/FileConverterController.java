59488c1cc1fe2fbd55d72267b4c2765591addbef
Charles <zhangchaoxu@gmail.com>
2025-03-24 13:17:09 +0800

    add file converter

--- file-converter/src/main/java/com/nb6868/onex/fileconverter/controller/FileConverterController.java
+++ file-converter/src/main/java/com/nb6868/onex/fileconverter/controller/FileConverterController.java
@@ -5,9 +5,17 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 在线预览实现https://juejin.cn/post/7264609364672512056
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
 @RestController
 @RequestMapping("/file/converter")
 @Validated
 @Tag(name = "文件转换器")
 public class FileConverterController {
+
+
+
 }
