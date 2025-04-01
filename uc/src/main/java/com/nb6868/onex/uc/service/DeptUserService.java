ba6499748bb46f766a1b74de6425ba13e094e9cd
Charles <zhangchaoxu@gmail.com>
2025-04-01 17:00:16 +0800

    release 4.4.71, 修复deptuser关系表处理对userids.size=1时候的错误处理

--- uc/src/main/java/com/nb6868/onex/uc/service/DeptUserService.java
+++ uc/src/main/java/com/nb6868/onex/uc/service/DeptUserService.java
@@ -99,7 +99,7 @@ public class DeptUserService extends EntityService<DeptUserDao, DeptUserEntity> 
             // 删除非指定范围内的其它的关系
             remove(lambdaQuery().eq(DeptUserEntity::getDeptId, deptId)
                     .notIn(userIds.size() > 1, DeptUserEntity::getUserId, userIds)
-                    .ne(userIds.size() == 1, DeptUserEntity::getUserId, userIds)
+                    .ne(userIds.size() == 1, DeptUserEntity::getUserId, userIds.get(0))
                     .eq(DeptUserEntity::getType, type)
                     .getWrapper());
         }
